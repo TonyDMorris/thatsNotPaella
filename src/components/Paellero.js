725475ac65a51cbde82d2d4f6aadef2738f580de
TonyDMorris <anthonymorris86@googlemail.com>
2019-03-05 11:55:10 +0000

    need to add styles

--- src/components/Paellero.js
+++ src/components/Paellero.js
@@ -9,29 +9,14 @@ const types = {
 function collect(connect, monitor) {
    
     return {
-        
-    connectDropTarget: connect.dropTarget()
+        connectDropTarget: connect.dropTarget()
     }
    }
-
-  function drop(props, monitor, component) {
-    if (monitor.didDrop()) {
-      // If you want, you can check whether some nested
-      // target already handled drop
-      return 'hello world'
-    }}
-
-
-
-    
-    
-    
-
 
 export class Paellero extends Component {
     render() {
         
-        const {drop, connectDropTarget } = this.props;
+        const {connectDropTarget} = this.props;
 
         return connectDropTarget(
 <div style={style}>this is where the paellero will be</div>
@@ -46,4 +31,4 @@ const style = {
     backgroundColor:'blue',
     gridRow: '1 / span 1'
 };
-export default DropTarget(types.food, {drop}, collect)(Paellero)
+export default DropTarget(types.food, {}, collect)(Paellero)
