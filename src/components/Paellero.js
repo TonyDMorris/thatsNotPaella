0e4a5ee3e1a8f559fd86acb90e58d27f16f6fefc
TonyDMorris <anthonymorris86@googlemail.com>
2019-03-08 08:42:33 +0000

    chenaging style to background pealla

--- src/components/Paellero.js
+++ src/components/Paellero.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { DropTarget } from 'react-dnd';
-import backGround from '/home/tony/Documents/thatsnotpaella/src/components/svgs/paella-149635_1280.png'
+import background from '/home/tony/Documents/thatsnotpaella/src/components/svgs/paella-fuse-5741e40c5f9b58723db9a309.jpg'
 
 
 const types = {
@@ -19,39 +19,24 @@ export class Paellero extends Component {
         const {connectDropTarget} = this.props;
 
         return connectDropTarget(
-<div style={style}><div style={imgStyle}  alt='a paella'></div><h1 style={textStyle}>Add things until you ruin the Paella</h1></div>
+<div style={style}></div>
         )
     }
 };
 
-const imgStyle ={
-    width:'100%',
-    height:'100%',
-    border: 'none',
-    display: 'block',
-    margin: '0 auto',
-    gridRow:'1 / span 1',
-    backgroundImage:`url('${backGround}')`,
-    backgroundSize:'contain',
-    backgroundRepeat:'no-repeat',
-    backgroundPosition:'center'
-   
-}
-const textStyle = {
-    
-    gridRow:'2 / span 1'
-    
-}
+
+
 const style = {
-    display:'grid',
-    textAlign:'center',
-    gridTemplateRows:'auto auto',
-    backgroundColor:'#008c72',
+    backgroundSize:'cover',
+    backgroundPosition:'center',
+    backgroundImage:`url('${background}')`,
+    boxShadow:'inset 10px 10px 0px 0px #005840',
+    
     border:'solid 10px #003840',
     borderRadius:'100%',
     gridColumn:'2 / span 1',
     gridRow: '1 / span 1',
-    verticalAlign:'text-top'
+    
 };
     
 export default DropTarget(types.food, {}, collect)(Paellero)
